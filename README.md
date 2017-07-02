1d462e93ebe42f9a5a4500189a33bb7bd5af53a6
daizeng1984 <daizeng1984@gmail.com>
2017-07-02 15:59:35 +0800

    Added basic current word feeling lucky reflink generator

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Introduction
-TODO...
+I feel I need a quick keyword linking tools in Vim so that I could keep writing while providing reflink. But I'm too lazy to grab link by myself and I trust Google most of the time. Therefore, this plugin is written to feed my need! It just does the job for me.
 
+# How
+Put your cursor on the word and make command `:FeelingLuckyReflink` and you're done.
+
+```
+Jekyll --> [Jekyll](https://jekyllrb.com/)
+```
+
+# TODO
--- rplugin/python/feeling-lucky.py
+++ rplugin/python/feeling-lucky.py
@@ -1,11 +1,22 @@
 import neovim
+import requests
 
 @neovim.plugin
 class Main(object):
     def __init__(self, vim):
         self.vim = vim
 
-    @neovim.function('FeelingLuckyReflink')
+    @neovim.command('FeelingLuckyReflink', nargs='*')
     def feelingLuckyReflink(self, args):
-        self.vim.command('echo "hello from Feeling Lucky"')
+        word = self.vim.eval('expand("<cword>")') 
+        currentline = self.vim.current.line 
+        self.vim.current.line = self.updateLine(currentline, word, 1)
+    def updateLine(self, line, word, maxnum):
+        if word in line:
+            # get url
+            var = requests.get(r'http://www.google.com/search?q="' + word + '"&btnI&pws=0&gl=us&gws_rd=cr')
+            line = line.replace(word, '[' + word + '](' + var.url + ')', maxnum)
+            #self.vim.command('echo "' + word + ", " + var.url + ", " + line + '"') # + args[0] + '"')
+        return line
+
 
