823cc408c34449e5c02e68941598a52a22464f8a
daizeng1984 <daizeng1984@gmail.com>
2017-08-05 20:49:36 +0800

    Added FeelingLuckyEverywhere command! Added shelve storage to allow dump these reflink words to local disk and then use it later with FeelingLuckyEverywhere

--- rplugin/python/feeling-lucky.py
+++ rplugin/python/feeling-lucky.py
@@ -1,10 +1,43 @@
 import neovim
+import os 
+import shelve
+import re
 from googleapiclient.discovery import build
+
+class LuckyStay(object):
+    def __init__(self):
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        self.db_path = dir_path + 'feelinglucky'
+
+    # TODO: python2 problem with 'with'?
+    def put(self, word, url):
+        #with shelve.open(self.db_path) as db:
+        db = shelve.open(self.db_path)
+        db[word] = url
+        db.close()
+
+    def get(self, word):
+        #with shelve.open(self.db_path) as db:
+        #    return db[word]
+        db = shelve.open(self.db_path)
+        ret = db[word]
+        db.close()
+        return ret
+        
+    def getAll(self):
+        #with shelve.open(self.db_path) as db:
+        #    return dict(db)
+        db = shelve.open(self.db_path)
+        ret = dict(db)
+        db.close()
+        return ret
+
 
 @neovim.plugin
 class Main(object):
     def __init__(self, vim):
         self.vim = vim
+        self.luckyStay = LuckyStay();
 
     @neovim.command('FeelingLuckyReflink', nargs='*')
     def feelingLuckyReflink(self, args):
@@ -41,8 +74,35 @@ class Main(object):
                 url = res.get('items', [{'link':''}])[0].get('link')
 
             # get url
-            line = line.replace(word, '[' + word + '](' + url + ')', maxnum)
+            line = self.updateBuffer(line, word, url, maxnum)
+            # upsert
+            self.luckyStay.put(word, url)
             #self.vim.command('echo "' + word + ", " + var.url + ", " + line + '"') # + args[0] + '"')
         return line
+    def updateBuffer(self, doc, word, url, maxnum):
+        if not doc:
+            return doc
+        return re.sub(r"(" + word + ")", '[' + word + '](' + url + ')', doc, count=maxnum)
+
+    @neovim.command('FeelingLuckyEverywhere', nargs='*')
+    def luckyEveryWhere(self, args):
+        d = self.luckyStay.getAll()
+        i = 0
+        taboo = []
+        for l in self.vim.current.buffer:
+            taboo = taboo + list(re.findall(r"(?:[[\s]*)([\S]+)(?:[\s]*])(?:[\s]*\([^)]*\))", l))
+        for t in taboo:
+            d.pop(t) 
+        #self.vim.command('echo "lucky everywhere: ' + str(d) + '"')
+        
+        for l in self.vim.current.buffer:
+            for word in d:
+                l = self.updateBuffer(l, word, d[word], 1)
+            #self.vim.command('echo "lucky everywhere: ' + str(l) + '"')
+            if l != self.vim.current.buffer[i]:
+                self.vim.current.buffer[i] = l;
+                d.pop(word)
+            i = i + 1
+
 
 
