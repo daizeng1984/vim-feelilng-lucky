ac1080fffb2cd79665d32dd2c36628a7747d8c51
daizeng1984 <daizeng1984@gmail.com>
2019-01-14 00:45:07 -0800

    Added clear method and refactored code

--- rplugin/python3/feeling-lucky.py
+++ rplugin/python3/feeling-lucky.py
@@ -1,8 +1,9 @@
 import neovim
-import os 
+import os
 import shelve
 import re
 from googleapiclient.discovery import build
+
 
 class LuckyStay(object):
     def __init__(self):
@@ -11,21 +12,26 @@ class LuckyStay(object):
 
     # TODO: python2 problem with 'with'?
     def put(self, word, url):
-        #with shelve.open(self.db_path) as db:
+        # with shelve.open(self.db_path) as db:
         db = shelve.open(self.db_path)
         db[word] = url
         db.close()
 
     def get(self, word):
-        #with shelve.open(self.db_path) as db:
+        # with shelve.open(self.db_path) as db:
         #    return db[word]
         db = shelve.open(self.db_path)
         ret = db[word]
         db.close()
         return ret
-        
+
+    def clear(self):
+        db = shelve.open(self.db_path)
+        db.clear()
+        db.close()
+
     def getAll(self):
-        #with shelve.open(self.db_path) as db:
+        # with shelve.open(self.db_path) as db:
         #    return dict(db)
         db = shelve.open(self.db_path)
         ret = dict(db)
@@ -37,7 +43,7 @@ class LuckyStay(object):
 class Main(object):
     def __init__(self, vim):
         self.vim = vim
-        self.luckyStay = LuckyStay();
+        self.luckyStay = LuckyStay()
 
     @neovim.command('FeelingLuckyReflink', nargs='*')
     def feelingLuckyReflink(self, args):
@@ -46,9 +52,9 @@ class Main(object):
         (lnum2, col2) = buf.mark('>')
         lines = self.vim.eval('getline({}, {})'.format(lnum1, lnum2))
         word = ""
-        if lines :
+        if lines:
             # Quick fix...
-            if len(lines) > 1 :
+            if len(lines) > 1:
                 lines[0] = lines[0][col1:]
                 lines[-1] = lines[-1][:col2]
                 word = "\n".join(lines)
@@ -56,54 +62,57 @@ class Main(object):
                 word = lines[0][col1:col2]
         # Be smart
         if not word:
-            word = self.vim.eval('expand("<cword>")') 
-        currentline = self.vim.current.line 
+            word = self.vim.eval('expand("<cword>")')
+        currentline = self.vim.current.line
         self.vim.current.line = self.updateLine(currentline, word, 1)
+
     def updateLine(self, line, word, maxnum):
         if word in line:
-            self.service = build( 
+            self.service = build(
                 "customsearch", "v1",
                 developerKey="AIzaSyCYtnzDWqzPYE6LTtNyuxEZ6pLy6DCkysQ")
-            
+
             res = self.service.cse().list(
                     q=word,
                     cx='014324005263008877010:zmwtfjahkfe',
                     ).execute()
             url = ""
-            if res :
-                url = res.get('items', [{'link':''}])[0].get('link')
+            if res:
+                url = res.get('items', [{'link': ''}])[0].get('link')
 
             # get url
             line = self.updateBuffer(line, word, url, maxnum)
             # upsert
             self.luckyStay.put(word, url)
-            #self.vim.command('echo "' + word + ", " + var.url + ", " + line + '"') # + args[0] + '"')
         return line
+
     def updateBuffer(self, doc, word, url, maxnum):
         if not doc:
             return doc
-        return re.sub(r"(" + word + ")", '[' + word + '](' + url + ')', doc, count=maxnum)
+        return re.sub(
+            r"(" + word + ")", '[' + word + '](' + url + ')', doc, count=maxnum
+        )
+
+    @neovim.command('FeelingLuckyClear', nargs='*')
+    def luckyClear(self, args):
+        self.luckyStay.clear()
 
     @neovim.command('FeelingLuckyEverywhere', nargs='*')
     def luckyEveryWhere(self, args):
         d = self.luckyStay.getAll()
         i = 0
-        # TODO: better to find all 
+        # TODO: better to find all
         taboo = []
         for l in self.vim.current.buffer:
-            taboo = taboo + list(re.findall(r"(?:[[\s]*)([\S]+)(?:[\s]*])(?:[\s]*\([^)]*\))", l))
+            taboo = taboo + list(re.findall(
+                r"(?:[[\s]*)([\S]+)(?:[\s]*])(?:[\s]*\([^)]*\))", l))
         for t in taboo:
-            d.pop(t, None) 
-        #self.vim.command('echo "lucky everywhere: ' + str(d) + '"')
-        
+            d.pop(t, None)
+
         for l in self.vim.current.buffer:
             for word in d:
-                l = self.updateBuffer(l, word, d[word], 1)
-            #self.vim.command('echo "lucky everywhere: ' + str(l) + '"')
-            if l != self.vim.current.buffer[i]:
-                self.vim.current.buffer[i] = l;
+                line = self.updateBuffer(l, word, d[word], 1)
+            if line != self.vim.current.buffer[i]:
+                self.vim.current.buffer[i] = line
                 d.pop(word, None)
             i = i + 1
-
-
-
